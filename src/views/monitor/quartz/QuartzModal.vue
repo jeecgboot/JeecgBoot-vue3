518d9cb2d0fac741db454b073d9db6f1b3f96af0
zhangdaiscott <zhangdaiscott@163.com>
2023-10-18 16:54:41 +0800

    【issues/790】弹窗内文本框不居中问题

--- src/views/monitor/quartz/QuartzModal.vue
+++ src/views/monitor/quartz/QuartzModal.vue
@@ -18,6 +18,11 @@
     // labelWidth: 150,
     schemas: formSchema,
     showActionButtonGroup: false,
+    // update-begin--author:liaozhiyang---date:20231017---for：【issues/790】弹窗内文本框不居中问题
+    labelWidth: 100,
+    labelCol: null,
+    wrapperCol: null,
+    // update-end--author:liaozhiyang---date:20231017---for：【issues/790】弹窗内文本框不居中问题
   });
   //表单赋值
   const [registerModal, { setModalProps, closeModal }] = useModalInner(async (data) => {
