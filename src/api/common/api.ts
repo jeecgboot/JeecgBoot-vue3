545b1204b909080b88a59be89b9fc9c39f85002f
ykcory <yacoda@qq.com>
2023-12-28 05:27:13 +0800

    fix: 去除 downloadFile 中 IE 浏览器的支持
    vue3版本已经不支持ie浏览器

--- src/api/common/api.ts
+++ src/api/common/api.ts
@@ -109,19 +109,15 @@ export const downloadFile = (url, fileName?, parameter?) => {
       message.warning('文件下载失败');
       return;
     }
-    if (typeof window.navigator.msSaveBlob !== 'undefined') {
-      window.navigator.msSaveBlob(new Blob([data]), fileName);
-    } else {
-      let url = window.URL.createObjectURL(new Blob([data]));
-      let link = document.createElement('a');
-      link.style.display = 'none';
-      link.href = url;
-      link.setAttribute('download', fileName);
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link); //下载完成移除元素
-      window.URL.revokeObjectURL(url); //释放掉blob对象
-    }
+    const url = window.URL.createObjectURL(new Blob([data]));
+    const link = document.createElement('a');
+    link.style.display = 'none';
+    link.href = url;
+    link.setAttribute('download', fileName);
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link); //下载完成移除元素
+    window.URL.revokeObjectURL(url); //释放掉blob对象
   });
 };
 
